2917d4fd9a34387a89b3e6b6ab3f626aa77b06fa
Ihsanulisra Maulana <73300775+RaveNewt@users.noreply.github.com>
2023-06-27 20:44:16 +0700

    Initial Commit

---
+++ EWallet/EWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  EWalletApp.swift
+//  EWallet
+//
+//  Created by Ihsanulisra Maulana on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct EWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
